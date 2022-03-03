13a163bd86575449da0f3caea420980356b88015
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2022-03-03 16:53:02 +0900

    ログインフォーム作成、バリデーション仮設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// ログイン画面ルーティング用Router設定
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ログイン画面表示
+Route::get('/', [AuthController::class,
+'showLogin'])->name('showLogin');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// ログイン処理
+Route::post('login', [AuthController::class,
+'login'])->name('login');
